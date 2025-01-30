d3c2b331a2b4291e8756a8a92e1967f5af025737
ct-ctech <159247033+ct-ctech@users.noreply.github.com>
2025-01-30 21:47:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,63 +1,18 @@
-{
-    const wrapper = document.querySelector('.gallery-celebrations-carousel-wrapper');
-    const slides = document.querySelectorAll('.gallery-celebrations-carousel-slide');
-
-    let currentIndex = 0;
-
-    function nextSlide() {
-      currentIndex = (currentIndex + 1) % slides.length;
-      updateCarousel();
-    }
-
-    function updateCarousel() {
-      const translateValue = -currentIndex * (100 / slides.length) + '%';
-      wrapper.style.transform = 'translateX(' + translateValue + ')';
-    }
-
-    // Automatically advance to the next slide every 2 seconds (adjust as needed)
-    setInterval(nextSlide, 3000);
-}
-
-
-
-{
-    const wrapper = document.querySelector('.gallery-ctech23-carousel-wrapper');
-    const slides = document.querySelectorAll('.gallery-ctech23-carousel-slide');
-
-    let currentIndex = 0;
-
-    function nextSlide() {
-      currentIndex = (currentIndex + 1) % slides.length;
-      updateCarousel();
-    }
-
-    function updateCarousel() {
-      const translateValue = -currentIndex * (100 / slides.length) + '%';
-      wrapper.style.transform = 'translateX(' + translateValue + ')';
-    }
-
-    // Automatically advance to the next slide every 3 seconds
-    setInterval(nextSlide, 2000);
-}
-    
-    
-    
-
-/**Scroll animation */
-const observer= new IntersectionObserver((entries)=>{
-    entries.forEach((entry)=>{
-        console.log(entry)
-        if (entry.isIntersecting){
-            entry.target.classList.add('show');
-        }
-        else{
-            entry.target.classList.remove('show');
-        }
+document.addEventListener("DOMContentLoaded", () => {
+    const menuToggle = document.getElementById("menu-toggle");
+    const menuOpenIcon = document.getElementById("menu-open-icon");
+    const menuCloseIcon = document.getElementById("menu-close-icon");
+    const mobileMenu = document.getElementById("mobile-menu");
+  
+    menuToggle.addEventListener("click", () => {
+      const isHidden = !mobileMenu.classList.contains("hidden");
+      
+      // Toggle the menu
+      mobileMenu.classList.toggle("hidden");
+  
+      // Toggle icons
+      menuOpenIcon.classList.toggle("hidden", !isHidden);
+      menuCloseIcon.classList.toggle("hidden", isHidden);
     });
-});
-
-const hiddenElements=document.querySelectorAll('.hidden');
-hiddenElements.forEach((el)=>observer.observe(el));
-
-
-
+  });
+  
